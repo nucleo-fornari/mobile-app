3f00d1452e835dfbae0c13c90d0a39ef2c9d5290
juliadmoura <julia.moura@sptech.school>
2025-03-07 20:09:12 -0300

    feat: componentes e tela inicial criada

---
+++ app/src/main/java/com/example/nucleofornari/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.nucleofornari
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.nucleofornari.ui.theme.AzulPrincipal
+import com.example.nucleofornari.ui.theme.NucleoFornariTheme
+import com.example.nucleofornari.ui.theme.components.TextButton
+import com.example.nucleofornari.ui.theme.components.WhiteButton
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NucleoFornariTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = AzulPrincipal)
+    ) {
+        Column(
+            modifier = modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logobranco),
+                contentDescription = "Logo Núcleo Fornari",
+                modifier = Modifier.size(300.dp),
+//                contentScale = ContentScale.Crop
+            )
+            
+            Column(
+//                modifier = modifier
+//                    .align(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                WhiteButton("Entrar", handleCLick = ({}))
+                TextButton("Ainda não tem uma conta?", Color.White)
+            }
+
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NucleoFornariTheme {
+        Greeting()
+    }
+}
---
+++ app/src/main/java/com/example/nucleofornari/ui/theme/components/Buttons.kt
@@ -0,0 +1,46 @@
+package com.example.nucleofornari.ui.theme.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.nucleofornari.ui.theme.AzulPrincipal
+
+@Composable
+fun WhiteButton(text: String, handleCLick: () -> Unit) {
+    Button(
+        onClick = { handleCLick },
+        modifier = Modifier.size(302.dp, 60.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.White,
+            contentColor = AzulPrincipal
+        ),
+        shape = RoundedCornerShape(6.dp)
+    ) {
+        Text(
+            text = text
+        )
+    }
+}
+
+@Composable
+fun TextButton(text: String, color: Color) {
+    Button(
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent
+        ),
+        elevation = null
+    ) {
+        Text(
+            text = text,
+            color = color
+        )
+    }
+}
+
